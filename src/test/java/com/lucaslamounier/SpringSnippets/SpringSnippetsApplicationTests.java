d38ce4f7af79a31748f66f7acf76c4e892014f3c
lucaslamounier08 <lucaslamounier08@gmail.com>
2025-01-23 13:10:22 -0300

    first commit: spring initializer project

---
+++ src/test/java/com/lucaslamounier/SpringSnippets/SpringSnippetsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucaslamounier.SpringSnippets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSnippetsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
