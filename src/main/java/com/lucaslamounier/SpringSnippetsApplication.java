8ce39f1602ab764bb136cb6979c0e132052ba3f3
lucaslamounier08 <lucaslamounier08@gmail.com>
2025-01-23 13:13:44 -0300

    fix package name

--- src/main/java/com/lucaslamounier/SpringSnippetsApplication.java
+++ src/main/java/com/lucaslamounier/SpringSnippetsApplication.java
@@ -1,4 +1,4 @@
-package com.lucaslamounier.SpringSnippets;
+package com.lucaslamounier;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/lucaslamounier/SpringSnippetsApplicationTests.java
+++ src/test/java/com/lucaslamounier/SpringSnippetsApplicationTests.java
@@ -1,4 +1,4 @@
-package com.lucaslamounier.SpringSnippets;
+package com.lucaslamounier;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
