77cc73db770cf7bb6b7d2185af1fd22a947b4634
lucaslamounier08 <lucaslamounier08@gmail.com>
2025-01-23 13:50:15 -0300

    chore: fix ActuatorHealthRestAssuredIntegrationTest

--- src/test/java/com/lucaslamounier/actuator/ActuatorHealthRestAssuredIntegrationTest.java
+++ src/test/java/com/lucaslamounier/actuator/ActuatorHealthRestAssuredIntegrationTest.java
@@ -1,13 +1,23 @@
 package com.lucaslamounier.actuator;
 
 import io.restassured.RestAssured;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
 
 import static org.hamcrest.Matchers.equalTo;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class ActuatorHealthRestAssuredIntegrationTest {
+
+    @LocalServerPort
+    private int port;
+
+    @BeforeEach
+    void setUp() {
+        RestAssured.port = port;
+    }
 
     @Test
     void healthEndpointShouldReturnUp() {
