1cb4b188ad50a618b4ae96a09d89b9667c959220
lucaslamounier08 <lucaslamounier08@gmail.com>
2025-01-24 11:58:10 -0300

    feat: add Jpa Auditing - auto creationDate and lastModifiedDate to entity

--- src/main/java/com/lucaslamounier/usecase/book/adapters/out/persistence/BookEntity.java
+++ src/main/java/com/lucaslamounier/usecase/book/adapters/out/persistence/BookEntity.java
@@ -2,18 +2,24 @@ package com.lucaslamounier.usecase.book.adapters.out.persistence;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.Data;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 
+@Data
 @Entity
 @Table(name = "book")
-@Data
+@EntityListeners(AuditingEntityListener.class)
 public class BookEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -30,4 +36,12 @@ public class BookEntity {
     private String isbn;
 
     private BigDecimal price;
+
+    @CreatedDate
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdDate;
+
+    @LastModifiedDate
+    @Column(nullable = false)
+    private LocalDateTime lastModifiedDate;
 }
--- src/main/resources/db/migration/V1__create_book_table.sql
+++ src/main/resources/db/migration/V1__create_book_table.sql
@@ -4,5 +4,7 @@ CREATE TABLE book (
     author VARCHAR(255) NOT NULL,
     published_date DATE,
     isbn VARCHAR(20),
-    price DECIMAL(10, 2)
+    price DECIMAL(10, 2),
+    created_date DATE,
+    last_modified_date DATE
 );
